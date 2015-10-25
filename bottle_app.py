3ad66936e26154f3ecb9c1a49d753da18ad43cc7
vincent <applekey@gmail.com>
2015-10-25 13:15:16 -0700

    including static files

--- bottle_app.py
+++ bottle_app.py
@@ -1,10 +1,15 @@
 
 # A very simple Bottle Hello World app for you to get started with...
-from bottle import default_app, route
+from bottle import default_app, route,run,static_file
 
 @route('/')
 def hello_world():
     return 'Hello from Bottle!'
 
+@route('/static/<filename>')
+def server_static(filename):
+    return static_file(filename, root='static')
+
 application = default_app()
 
+#run(host='localhost', port=8080)
