3e372502bb61480c7f0be0e0d11f8cf8a5011c99
vincent <applekey@gmail.com>
2016-01-02 13:05:32 -0800

    Starting template

--- bottle_app.py
+++ bottle_app.py
@@ -4,12 +4,16 @@ from bottle import default_app, route,run,static_file
 
 @route('/')
 def hello_world():
-    return 'Hello from Bottle!'
+    return static_file('start.html', root='static/html')
 
 @route('/shtml/<filename>')
 def server_static(filename):
     return static_file(filename, root='static/html')
 
+@route('/template/<filepath:path>')
+def server_static(filepath):
+    return static_file(filepath, root='static/startbootstrap-sb-admin-2-gh-pages/')
+
 application = default_app()
 
-run(host='localhost', port=8080)
+run(host='localhost', port=8000)
