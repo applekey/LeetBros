2202ce986e996cbb2ffbbb0d1e718e9803967475
vincent <applekey@gmail.com>
2016-01-03 12:54:54 -0800

    including alternative bottle_app.py

--- bottle_app.py
+++ bottle_app.py
@@ -1,36 +1,9 @@
 
 # A very simple Bottle Hello World app for you to get started with...
-from bottle import default_app, route,run,static_file
+from bottle import default_app,run
 ## custom imports
-import sys
-sys.path.append("attachments/")
-from emailAdapter import *
-
-@route('/')
-def hello_world():
-    return static_file('start.html', root='static/html')
-
-@route('/shtml/<filename>')
-def server_static(filename):
-    return static_file(filename, root='static/html')
-
-@route('/js/<filepath:path>')
-def server_static(filepath):
-    return static_file(filepath, root='static/js/')
-
-@route('/template/<filepath:path>')
-def server_static(filepath):
-    return static_file(filepath, root='static/startbootstrap-sb-admin-2-gh-pages/')
-
-@route('/functions/<function>')
-def server_static(function):
-    if function == 'attachments':
-        emailad = emailAdapter('applekeyhousing@gmail.com','vancouver!@#')
-        emailad.connect()
-        attachments = emailad.listAttachments()
-        emailad.disconnect()
-        return attachments
+from routes import *
 
 application = default_app()
 
-run(reloader=True,debug=True, host='localhost', port=8001)
+run(reloader=True,debug=True, host='localhost', port=8002)
---
+++ routes.py
@@ -0,0 +1,29 @@
+from bottle import route,static_file
+import sys
+sys.path.append("attachments/")
+from emailAdapter import *
+
+@route('/')
+def hello_world():
+    return static_file('start.html', root='static/html')
+
+@route('/shtml/<filename>')
+def server_static(filename):
+    return static_file(filename, root='static/html')
+
+@route('/js/<filepath:path>')
+def server_static(filepath):
+    return static_file(filepath, root='static/js/')
+
+@route('/template/<filepath:path>')
+def server_static(filepath):
+    return static_file(filepath, root='static/startbootstrap-sb-admin-2-gh-pages/')
+
+@route('/functions/<function>')
+def server_static(function):
+    if function == 'attachments':
+        emailad = emailAdapter('applekeyhousing@gmail.com','vancouver!@#')
+        emailad.connect()
+        attachments = emailad.listAttachments()
+        emailad.disconnect()
+        return attachments
