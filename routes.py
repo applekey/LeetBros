539408266cd9c8300a3cf25175930b852902654b
Vincent <applekey@gmail.com>
2016-01-03 21:33:22 +0000

    including live version

--- routes.py
+++ routes.py
@@ -1,24 +1,24 @@
 from bottle import route,static_file
 import sys,os
-attachmentDirectory = os.path.join(os.getcwd(),'attachments')
+attachmentDirectory = os.path.join(u'/home/applekey/mysite', u'attachments/')
 sys.path.append(attachmentDirectory)
 from emailAdapter import *
 
 @route('/')
 def hello_world():
-    return static_file('start.html', root='static/html')
+    return static_file('start.html', root='/home/applekey/mysite/static/html')
 
 @route('/shtml/<filename>')
 def server_static(filename):
-    return static_file(filename, root='static/html')
+    return static_file(filename, root='/home/applekey/mysite/static/html')
 
 @route('/js/<filepath:path>')
 def server_static(filepath):
-    return static_file(filepath, root='static/js/')
+    return static_file(filepath, root='/home/applekey/mysite/static/js/')
 
 @route('/template/<filepath:path>')
 def server_static(filepath):
-    return static_file(filepath, root='static/startbootstrap-sb-admin-2-gh-pages/')
+    return static_file(filepath, root='/home/applekey/mysite/static/startbootstrap-sb-admin-2-gh-pages/')
 
 @route('/functions/<function>')
 def server_static(function):
