7d5161e52210d63635003eb4ce31d2b9f75ac16d
Vincent <applekey@gmail.com>
2016-01-06 18:25:57 +0000

    sql error

--- db/createOwed.sql
+++ db/createOwed.sql
@@ -2,5 +2,5 @@ CREATE TABLE owed_tbl(
    owed_id INT NOT NULL AUTO_INCREMENT,
    owed_people_id INT NOT NULL,
    owed_bill_id INT NOT NULL,
-   PRIMARY KEY ( owed_people_id )
+   PRIMARY KEY ( owed_id )
 );
