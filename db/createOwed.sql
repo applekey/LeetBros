2eefdd0063a3f32a133d25f80d3aec3d12b29f17
Shayak Dutta <shayak007@gmail.com>
2016-01-13 00:27:15 -0500

    create owed table change

--- db/createOwed.sql
+++ db/createOwed.sql
@@ -2,6 +2,6 @@ CREATE TABLE owed_tbl(
    owed_id INT NOT NULL AUTO_INCREMENT,
    owed_people_id INT NOT NULL,
    owed_bill_id INT NOT NULL,
-   paid INT not null default 0,
+   paid bool not null default false,
    PRIMARY KEY ( owed_id )
 );
