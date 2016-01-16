d8f27ea4c585b13bc267c130a745804658e88bb1
vincent <applekey@gmail.com>
2016-01-15 16:22:02 -0800

    view table

--- static/js/navigation.js
+++ static/js/navigation.js
@@ -15,5 +15,38 @@ function loadAddTenant(){
 
 function loadViewTenant(){
   $('#page-wrapper').empty();
-  $('#page-wrapper').load('shtml/viewTenant.html');
+  $('#page-wrapper').load('shtml/viewTenant.html',loadTenants);
 }
+
+function insertTableElements(data)
+{
+    var result = $.parseJSON( data )
+    //fix fix fix check for none case
+    var headerString = "<thead> <tr> <th>#</th>"
+
+    for (key in result[0])
+    {
+      headerString += '<th>' + key + '</th>'
+    }
+    headerString +=" </tr>  </thead> "
+    //insert in the data elements
+    headerString += '<tbody>'
+
+    for(i=0; i < result.length ;i++)
+    {
+      headerString += '<tr>  <th scope="row">'+i+'</th>'
+      for (key in result[i])
+      {
+        headerString += '<th>' + result[i][key] + '</th>'
+      }
+      headerString += '</tr>'
+    }
+
+    headerString += '</tbody>'
+    //console.log(headerString)
+    $("#billTable").append(headerString)
+};
+
+function loadTenants(){
+  $.get( "/functions/viewTenants", insertTableElements)
+};
