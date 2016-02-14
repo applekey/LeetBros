62c462d21900e2aa0269664296b5d2bf2ebe2658
Shayak Dutta <shayak007@gmail.com>
2016-02-14 17:05:58 -0500

    start server with beaker

--- bottle_app.py
+++ bottle_app.py
@@ -1,9 +1,21 @@
 
 # A very simple Bottle Hello World app for you to get started with...
-from bottle import default_app,run
+from bottle import default_app,run, app
 ## custom imports
 from routes import *
 
-application = default_app()
+from beaker.middleware import SessionMiddleware
 
-#run(reloader=True,debug=True, host='localhost', port=8002)
+session_opts = {
+    'session.type': 'cookie',
+    'session.data_dir': './session/',
+    'session.validate_key' : 'validateplz',
+    'session.encrypt_key' : 'encryptplz',
+    'session.auto': True
+}
+
+app = SessionMiddleware(app(), session_opts)
+
+#application = default_app()
+
+run(app=app)#, reloader=True,debug=True, host='localhost', port=8080)
