434d29ef9c5233028f60559609766f79a932c2fa
vincent <applekey@gmail.com>
2016-03-05 21:01:44 -0800

    more file org changes

--- db/tables/Bill_tbl.sql
+++ db/tables/Bill_tbl.sql
@@ -4,6 +4,7 @@ CREATE TABLE Bill(
    Description NVARCHAR(500),
    Amount FLOAT NOT NULL,
    DueDate DATE NOT NULL,
-   UserId varchar(36) NOT NULL,
+   BillIssuerId varchar(36) NOT NULL,
+   BillPayeeId varchar(36) NOT NULL,
    PRIMARY KEY ( BillId )
 );
--- db/tables/createOwed.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE owed_tbl(
-   owed_id INT NOT NULL AUTO_INCREMENT,
-   owed_people_id INT NOT NULL,
-   owed_bill_id INT NOT NULL,
-   paid bool not null default false,
-   userId CHAR(16) NOT NULL,
-   PRIMARY KEY ( owed_id )
-);
